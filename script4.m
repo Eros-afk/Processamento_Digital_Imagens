eb4104a577e65a79836ebc03b91d81895756235b
Eros-afk <erosryansimette@alu.ufc.br>
2025-12-01 16:26:26 -0300

    temino script4

--- script4.m
+++ script4.m
@@ -1,1 +1,17 @@
-AAAA
+clear all
+close all
+clc
+pkg load image
+
+%Leitura da imagem
+img = imread('Elefante.jpg');
+figure,imshow(img);
+figure,imhist(img);
+
+y = 1 * double(img).^0.2;
+figure,imshow(y,[]);
+set(gca,'FontSize',12,'fontname','Arial');
+
+y = imadjust(img,[],[],0.2);
+figure,imshow(y,[]);
+set(gca,'FontSize',12,'fontname','Arial');
